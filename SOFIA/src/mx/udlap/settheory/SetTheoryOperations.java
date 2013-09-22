76cff0ab2962f330eb83807e0795aab9aad13b38
chidmdxx <rachid@outlook.com>
2013-09-22 18:55:13 -0500

    First time the project as an Android app

---
+++ SOFIA/src/mx/udlap/settheory/SetTheoryOperations.java
@@ -0,0 +1,41 @@
+package mx.udlap.settheory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SetTheoryOperations {
+	public static List<String> Union (List<List<String>> param)
+	{
+		//didn't use List.addall to prevent repeated values, something that doesn't happen in sets
+		List<String> toReturn=new ArrayList<String>();
+		for (List<String> set : param)
+		{
+			for(String value : set)
+			{
+				if(!toReturn.contains(value))
+				{
+					toReturn.add(value);
+				}
+			}
+		}
+		return toReturn;
+	}
+	
+	public static List<String> Diference (List<String> minuend, List<String> subtrahend )
+	{
+		minuend.removeAll(subtrahend);
+		return minuend;
+	}
+	
+	public static List<String> Intersection (List<List<String>> param)
+	{
+		//didn't use List.addall to prevent repeated values, something that doesn't happen in sets
+		List<String> toReturn=new ArrayList<String>();
+		toReturn.addAll(param.get(0));
+		for (List<String> set : param)
+		{
+			toReturn.retainAll(set);
+		}
+		return toReturn;
+	}
+}
