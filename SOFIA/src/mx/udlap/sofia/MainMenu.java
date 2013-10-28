3a162b6ed74ac9e9689a85108ce1e135f0bdab21
Ulysses <lincerom.ulys@uwlax.edu>
2013-10-28 09:07:12 -0600

    Main Menu

--- SOFIA/src/mx/udlap/sofia/MainMenu.java
+++ SOFIA/src/mx/udlap/sofia/MainMenu.java
@@ -1,14 +1,10 @@
 package mx.udlap.sofia;
 
-import com.example.sofia.*;
-import com.example.sofia.Theory_description;
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 public class MainMenu extends Activity {
